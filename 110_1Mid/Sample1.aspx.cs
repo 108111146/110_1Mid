2bd53861142dcca8c60c9d41f44dabfbafcee4e8
齊佑 張 <108111146@mail.aeust.edu.tw>
2021-11-12 09:53:28 +0800

    完成a部分

--- 110_1Mid/Sample1.aspx.cs
+++ 110_1Mid/Sample1.aspx.cs
@@ -8,7 +8,7 @@ using System.Web.UI.WebControls;
 namespace _110_1Mid {
     public partial class Sample1 : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-
+            
         }
 
         // To generate a 4 digital number
